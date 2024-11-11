9a8461306e41839573c1277378b5f6f9fb142cb3
farrukh <f.buriyev@newuu.uz>
2024-11-11 18:18:48 +0500

    Update problem2.cpp

--- problem2.cpp
+++ problem2.cpp
@@ -1,53 +1,51 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-    int m,n,sum;
-    float avrg_column,avrg_row;
-    cin>>m>>n;
+int main() {
+    int m, n, sum = 0;
+    float avrg_column, avrg_row;
+    cin >> m >> n;
     int arr[m][n];
-    for(int i=0;i<m;i++){
-        for(int j=0;j<n;j++){
-            cin>>arr[i][j];
-            cout<<arr[i][j]<<"  ";
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> arr[i][j];
         }
-        cout<<endl;
+        cout << endl;
     }
-    
-    for(int i=0;i<m;i++){
-        for(int j=0;j<n;j++){
-            sum+=arr[i][j];
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            sum += arr[i][j];
         }
-        if(avrg_column!=0 && avrg_column!=sum/n){
-            cout<<"No";
+
+        if (avrg_row != 0 && avrg_row != sum / n) {
+            cout << "No";
             return 0;
         }
-        avrg_column = sum/n ;
-        cout<<avrg_column<<" avrg_column";
-        sum=0;
-        
+        avrg_row = sum / n;
+
+        sum = 0;
     }
-    
-    for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
-            sum+=arr[i][j];
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            sum += arr[j][i];
         }
-        if(avrg_row!=0 && avrg_row!=sum/m){
-            cout<<"No";
+
+        if (avrg_column != 0 && avrg_column != sum / n) {
+            cout << "No";
             return 0;
         }
-        avrg_row = sum/m ;
-        cout<<avrg_row<<" avrg_row";
-        sum=0;
-        
+        avrg_column = sum / n;
+
+        sum = 0;
     }
-    if(avrg_row==avrg_column){
-        cout<<"Yes";
+
+    if (avrg_column == avrg_row) {
+        cout << "Yes";
         return 0;
     }
-    cout<<"No";
-    
-    
+
+    cout << "No";
+
+
     return 0;
 }
