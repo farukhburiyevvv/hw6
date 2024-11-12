9afb1ff55658b73bb36cb663cf5973af35568195
farrukh <f.buriyev@newuu.uz>
2024-11-12 11:23:06 +0500

    Update problem5.cpp

--- problem5.cpp
+++ problem5.cpp
@@ -1,18 +1,29 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main() {
-    int a,b,c,e,d,count;
-    // cin >> m;
-    cin>>a>>b>>c>>e>>d;
-
-    for (int i = 1; i <= d ; i++) {
-        if (i%a==0) count++;
-        if (i%b==0 && i%a!=0) count++;
-        if (i%c==0 && i%b==0 && i%a!=0) count++;
-        if (i%e==0 && i%b==0 && i%a!=0 && i%c!=0) count++;
+    int a, b, c, e, d;
+    cin >> a >> b >> c >> e >> d;
+    vector<bool> nums_dragon(d + 1, false);
+    for (int i = a; i <= d; i += a) {
+        nums_dragon[i] = true;
     }
-
-    cout<<count;
+    for (int i = b; i <= d; i += b) {
+        nums_dragon[i] = true;
+    }
+    for (int i = c; i <= d; i += c) {
+        nums_dragon[i] = true;
+    }
+    for (int i = e; i <= d; i += e) {
+        nums_dragon[i] = true;
+    }
+    int numDragons = 0;
+    for (int i = 1; i <= d; ++i) {
+        if (nums_dragon[i]) {
+            numDragons++;
+        }
+    }
+    cout << numDragons << endl;
     return 0;
 }
